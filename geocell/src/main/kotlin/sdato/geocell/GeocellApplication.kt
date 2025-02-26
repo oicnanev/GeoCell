d3f88444978b2a946d53c30cc488ff0a4e14ebf8
oicnanev <voicnanev@proton.me>
2025-02-26 15:35:59 +0000

    add project skeleton generated by spring initializr

---
+++ geocell/src/main/kotlin/sdato/geocell/GeocellApplication.kt
@@ -0,0 +1,11 @@
+package sdato.geocell
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeocellApplication
+
+fun main(args: Array<String>) {
+	runApplication<GeocellApplication>(*args)
+}
